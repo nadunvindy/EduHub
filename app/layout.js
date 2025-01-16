c0b1f625d6677cb4e75919a5648ed774029ad966
BALASURIYA Nadun <nadun.balasuriya@education.wa.edu.au>
2025-01-16 13:38:20 +0800

    Add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "../src/globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/react"
+
 
 export default function RootLayout({ children }) {
   return (
