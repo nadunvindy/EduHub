866049f080895691b048a58902c67ad151c735f0
BALASURIYA Nadun <nadun.balasuriya@education.wa.edu.au>
2025-01-16 15:00:15 +0800

    Add google analytics tag

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,18 @@ import "../src/globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Analytics } from "@vercel/analytics/react";
-import { GoogleTagManager } from '@next/third-parties/google' 
+import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
-        <GoogleTagManager gtmId="GTM-NQ5FRSZR"/>
+        <GoogleAnalytics
+          gaId="G-1GNX7VH5E3
+"
+        />
+        <GoogleTagManager gtmId="GTM-NQ5FRSZR" />
         <Analytics />
         <Header />
         <main className="flex-grow justify-center content-center">
