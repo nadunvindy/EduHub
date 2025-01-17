4e68e81188e16ed581762b1a535030479463ce51
BALASURIYA Nadun <nadun.balasuriya@education.wa.edu.au>
2025-01-17 14:02:15 +0800

    Add code for speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "../src/globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         />
         <GoogleTagManager gtmId="GTM-NQ5FRSZR" />
         <Analytics />
+        <SpeedInsights/>
         <Header />
         <main className="flex-grow justify-center content-center">
           {children}
