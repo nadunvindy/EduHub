5e7631127ace8f5b9f1b232aa32a7af9152429c6
Nadun Balasuriya <nadun.balasuriya@education.wa.edu.au>
2025-02-07 10:13:21 +0800

    Add custom ID Clarity script

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
-'use client'
+"use client";
 
 import "../src/globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script>window.clarity("identify", "{{ email }}")</script>
       <body className="flex flex-col min-h-screen">
         <GoogleAnalytics
           gaId="G-1GNX7VH5E3
@@ -18,7 +19,7 @@ export default function RootLayout({ children }) {
         />
         <GoogleTagManager gtmId="GTM-NQ5FRSZR" />
         <Analytics />
-        <SpeedInsights/>
+        <SpeedInsights />
         <Header />
         <main className="flex-grow justify-center content-center">
           {children}
